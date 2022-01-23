6d56e1c27e7956d41aeb51d04be6ab5553a3d1cd
Sanskar Gupta <sanskargupta15@gmail.com>
2022-01-23 17:10:58 +0000

    removed permutations, made some tweaks in color vector bit

--- code/graph.py
+++ code/graph.py
@@ -1,4 +1,7 @@
+# imports
 import numpy as np
+from tabulate import tabulate
+from itertools import permutations
 
 
 # function to print the lower diagonal
@@ -17,10 +20,10 @@ def lower(Matrix, row, col):
 
 
 matrix = np.array([[0, 0, 1, 0, 1],
-                  [0, 0, 1, 1, 1],
-                  [1, 1, 0, 0, 0],
-                  [0, 1, 0, 0, 0],
-                  [1, 1, 0, 0, 0]])
+                   [0, 0, 1, 1, 1],
+                   [1, 1, 0, 0, 0],
+                   [0, 1, 0, 0, 0],
+                   [1, 1, 0, 0, 0]])
 
 rows = 5
 cols = 5
@@ -32,32 +35,59 @@ lower(matrix, rows, cols)
 print('\n')
 
 color_vertex = np.array([1, 1, -1, -1, -1])
-print(bin(5))
 
+# for getting all the possible variations of the graph
+possible_colour_vectors = {
+    1: 1,
+    2: 1,
+    3: -1,
+    4: -1,
+    5: -1
+}
 
-result = np.matmul(matrix, color_vertex)
-print("The resulting 1x5 matrix is {}".format(result), '\n')
+perm = permutations(possible_colour_vectors.values())
+cv_list = []
 
-temp_array.append([-2, -3, 2, 1, 2])
+count = 0
+
+for items in list(perm):
+    if items in cv_list:
+        continue
+    else:
+        cv_list.append(items)
+        print(items)
+        count += 1
+
+print("There are {} umber of unique possible colour vectors.".format(count))
+
+for i in range(len(cv_list)):
+    color_vector = np.array(cv_list[i])
+    result = np.matmul(matrix, color_vector)
+    print("----------------------------------------------------")
+    print("The resulting 1x5 matrix is {}".format(result), '\n')
+
+# temp_array.append([-2, -3, 2, 1, 2])
 
 # check for existing same variations
-for item in temp_array:
-    if item in result:
-        print("This variation already exists for row: {}".format(result[item]))
-    else:
-        temp_array.append(result)
 
-# Checks for
-for i in range(len(result)):
-    if result[i] > 0:
-        print("{}, Since the result is positive, it indicates there are more +1 edges for Row".format(result[i], i + 1),
-              '\n')
-    elif result[i] < 0:
-        print("{}, Since the result is negative, it indicates there are more -1 edges for Row".format(result[i], i + 1),
-              '\n')
-    else:
-        print("{}, Since the result is 0, it indicates the number of edges for both colours are same".format(result[i],
-                                                                                                             '\n'),
-              i + 1)
+    for item in temp_array:
+        if item in result:
+            print("This variation already exists for row: {}".format(result[item]))
+        else:
+            temp_array.append(result)
+
+    # Checks for
+    for i in range(len(result)):
+        if result[i] > 0:
+            print("{}, Since the result is positive, it indicates there are more +1 edges for Row".format(result[i], i+1))
+        elif result[i] < 0:
+            print("{}, Since the result is negative, it indicates there are more -1 edges for Row".format(result[i], i+1))
+        else:
+            print("{}, Since the result is 0, it indicates the number of edges for both colours are same".format(result[i], i+1))
 
 # expected result = [−2,−3,2,1,2]
+
+# creating table
+table = [['graph number', 'no_of_vertices', 'loop', 'cycles', 'iteration_length'],
+         [12, 'John', 'Smith', 39], ['Mary', 'Jane', 25], ['Jennifer', 'Doe', 28]]
+print(tabulate(table, headers='firstrow', tablefmt='grid'))
