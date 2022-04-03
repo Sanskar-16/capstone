92eb78acea745e7493ee1154266099a73dd526c5
sanskar <sanskargupta15@gmail.com>
2022-04-03 16:54:30 +0100

    minor fixes along with adding documentation to the code

--- code/extra.py
+++ code/extra.py
@@ -30,36 +30,54 @@
 # print("Lower triangular matrix: ")
 # lower(matrix, n, n)
 # print('\n')
-from itertools import product
-cvllist = []
-cv_list = []
-
-
-def get_cv_list(rep):
-    counter = 0
-    for vector in product([1, -1], repeat=rep):
-        cvllist.append(list(vector))
-        cvllist[counter].insert(0, 1)
-        counter = counter + 1
-    print("There are {} number of colour vectors in the cv_list".format(counter))
-
-    return cvllist
-
-def get_cv_listt(rep):
-    counter = 0
-    for vector in product([1, -1], repeat=rep):
-        # print(roll)
-        cv_list.append(list(vector))
-        counter = counter + 1
-    print("There are {} number of colour vectors in the cv_list".format(counter))
-
-    return cv_list
-
-
-get_cv_list(4)
-get_cv_listt(5)
-
-print(cvllist)
-print(cv_list)
-
-
+# from itertools import product
+# cvllist = []
+# cv_list = []
+#
+#
+# def get_cv_list(rep):
+#     counter = 0
+#     for vector in product([1, -1], repeat=rep):
+#         cvllist.append(list(vector))
+#         cvllist[counter].insert(0, 1)
+#         counter = counter + 1
+#     print("There are {} number of colour vectors in the cv_list".format(counter))
+#
+#     return cvllist
+#
+# def get_cv_listt(rep):
+#     counter = 0
+#     for vector in product([1, -1], repeat=rep):
+#         # print(roll)
+#         cv_list.append(list(vector))
+#         counter = counter + 1
+#     print("There are {} number of colour vectors in the cv_list".format(counter))
+#
+#     return cv_list
+#
+#
+# get_cv_list(4)
+# get_cv_listt(5)
+#
+# print(cvllist)
+# print(cv_list)
+#
+#
+# CONVERTING GRAPH BIT STRINGS TO MATRICES
+# import numpy as np
+# k = [0, 1, 0, 1, 1, 0]
+#
+# n = int(input("enafdaj"))
+#
+# zeros = [[0] * n for _ in range(n)]
+#
+# counter = 0
+#
+# for i in range(1, n):
+#     for j in range(0, i):
+#         zeros[i][j] = k[counter]
+#         counter = counter + 1
+#
+# zeros = np.matrix(zeros)
+# zeros = zeros + np.matrix.transpose(zeros)
+# print(zeros)
--- code/graph.py
+++ code/graph.py
@@ -16,6 +16,7 @@ cv_list = []
 # result holds the value of the multiplication between the matrix and the colour vector
 result = 0
 next_color_vector = 0
+graph_no = 0
 temp_list = []
 graph_list = []
 graph_int_list = []
@@ -25,8 +26,8 @@ table = {'Graph number': ['Graph no.', 1],
          'starting colour vertex': ['Starting cv', 1],
          'ending colour vertex': ['Ending cv', 1],
          'loop': ['Loop', 1],
-         'step time': ['Step time', 1],
-         'cycle': ['Cycle', 1]
+         'cycle': ['Cycle', 1],
+         'step time': ['Step time', 1]
          }
 
 
@@ -36,21 +37,15 @@ def get_zero_matrix(x):
     print("The zero matrix for {} vertices is {}".format(x, zero))
 
 
-def check_connected(x):
-    for _ in x:
-        if nx.is_connected(x):
-            continue
-        else:
-            break
-
-
 def get_cv_list(rep):
     counter = 0
-    for vector in product([1, -1], repeat=rep-1):
+    for vector in product([1, -1], repeat=rep - 1):
         cv_list.append(list(vector))
         cv_list[counter].insert(0, 1)
         counter = counter + 1
     print("There are {} colour vectors in total for {} vertices".format(counter, n))
+    print(cv_list)
+    print('\n')
 
     return cv_list
 
@@ -61,17 +56,19 @@ def get_graph_int_list(rep):
         graph_int_list.append(list(graph))
         counter = counter + 1
     print("There are {} graphs in total for {} vertices".format(counter, n))
+    print(graph_int_list)
+    print('\n')
 
     return graph_int_list
 
 
-# function that calculates the ending colour vector
-# the cycle it loops on in case the iteration does go into a loop
-def calculate_length_of_cycle():
-    length = len(temp_list) - temp_list.index(next_color_vector)
-    table['ending colour vertex'].append(next_color_vector)
-    table['loop'].append('yes')
-    table['step time'].append(length)
+# checks whether the graph passed as a parameter is connected or not
+def check_connected(x):
+    for _ in x:
+        if nx.is_connected(x):
+            continue
+        else:
+            break
 
 
 # function checking for 0s in the color_vector and replacing them with i of the previous color_vector
@@ -81,13 +78,32 @@ def check_for_zeroes(x):
             x[k] = color_vector[k]
 
 
-def convert_binary_to_decimal():
-    strings = [str(integer) for integer in graph_array]
-    graph_string = "".join(strings)
-    # print("The graph number in binary is {}".format(graph_string))
-    graph_num_in_dec = int(graph_string, 2)
-    # print("The converted graph number from binary to decimal is {}".format(graph_num_in_dec))
-    table['Graph number'].append(graph_num_in_dec)
+# def convert_binary_to_decimal():
+#     strings = [str(integer) for integer in graph_array]
+#     graph_string = "".join(strings)
+#     # print("The graph number in binary is {}".format(graph_string))
+#     graph_num_in_dec = int(graph_string, 2)
+#     # print("The converted graph number from binary to decimal is {}".format(graph_num_in_dec))
+#     table['Graph number'].append(graph_num_in_dec)
+
+
+'''
+step calculates the number of steps it takes for the algorithm to reach the same vector after it starts looping a
+vector, if the algorithm loops on the same vector, it will show 1. Otherwise, step would be number of vectors between
+the starting loop vector and when it occurs again.
+
+whereas the length var, it calculates the number of steps it takes for teh algorithm to reach teh starting
+colour vector again in the colour vector list.
+'''
+
+
+def calculate_length_of_cycle():
+    step = len(temp_list) - temp_list.index(next_color_vector)
+    length = temp_list.index(next_color_vector) + 1
+    table['ending colour vertex'].append(next_color_vector)
+    table['loop'].append('yes')
+    table['step time'].append(step)
+    table['cycle'].append(length)
 
 
 def append_stuff_to_table():
@@ -95,19 +111,19 @@ def append_stuff_to_table():
     # convert_binary_to_decimal()
     table['starting colour vertex'].append(cv_list[i])
     table['Number of vertices'].append(n)
+    table['Graph number'].append(graph_no)
 
 
+# taking input from the user
 n = int(input("Enter the number of vertices you want to do this for - "))
 N = int(1 / 2 * n * (n - 1))
 
+# obtaining all the essential lists based on used input
 print("The number of vertices (n) is {} which implies n bit binary string (N) is {}".format(n, N))
 get_zero_matrix(n)
 get_graph_int_list(N)
-print(graph_int_list)
 get_cv_list(n)
-print(cv_list)
-print('\n')
-graph_no = 0
+
 
 for g in graph_int_list:
     zeroes = zero
@@ -127,7 +143,6 @@ for g in graph_int_list:
     if nx.is_connected(G):
         print("this graph is connected...")
         for i in range(len(cv_list)):
-            table['Graph number'].append(graph_no)
             pi = pi + 1
             temp_list.clear()
             count = 0
@@ -152,7 +167,7 @@ for g in graph_int_list:
     else:
         print("skipped this graph, because it is not connected.")
 
-#
+# converting all the computations in a tabular format
 table_data = tabulate(table, headers='firstrow', tablefmt='simple', showindex='always')
 filename = "..\\output\\output.csv"
 text_file = open(filename, "w+")
@@ -163,3 +178,10 @@ print("Program completed")
 print("--- %s seconds ---" % (time.time() - start_time))
 # adjacency matrix using a graph library in python
 # looking up properties of graphs like clique and centrality based on the graph's
+# run for 7/8 and look for a loop bigger than 2
+# data analysis on loop size and other properties for all the graphs(percent of all the loop size 1 graphs)
+# insights from teh above analysis
+# majority from colour 1 to colour 2
+# cycle - no of rows DONE
+# look for a dataset on which it could be implemented
+# include what else could have been added to the report
