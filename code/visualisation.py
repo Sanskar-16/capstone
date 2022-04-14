55a9254d4502215563a22b7a9c428326dc1adf67
sanskar <sanskargupta15@gmail.com>
2022-04-14 11:36:27 +0100

    updated the report with captioned images and new graphs

--- code/visualisation.py
+++ code/visualisation.py
@@ -18,8 +18,11 @@ class GraphVisualization:
     def visualize(self):
         G = nx.Graph()
         G.add_edges_from(self.visual)
-        color_list = ["white", "black", "white", "black", "white"]
-        nx.draw_networkx(G, node_color=color_list, with_labels=True, font_color='red')
+        color_list = ["red", "blue", "red", "blue", "red", "red", "blue"]
+        color_list = ["red", "green", "red", "yellow", "yellow", "green", "red"]
+        # color_list = ["white", "black", "white", "black", "white"]
+        nx.draw_networkx(G, node_color=color_list, node_size=400, alpha=0.9, with_labels=True,
+                         font_color='black')
         plt.show()
 
 
@@ -30,5 +33,8 @@ G.addEdge('B', 'E')
 G.addEdge('A', 'E')
 G.addEdge('A', 'C')
 G.addEdge('C', 'B')
+G.addEdge('E', 'F')
+G.addEdge('F', 'G')
+G.addEdge('G', 'C')
 
 G.visualize()
