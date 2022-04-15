b2452d4d4964512235dcc02a703e88492ef9008d
sanskar <sanskargupta15@gmail.com>
2022-04-15 05:50:31 +0100

    major changes on report

--- code/visualisation.py
+++ code/visualisation.py
@@ -18,23 +18,17 @@ class GraphVisualization:
     def visualize(self):
         G = nx.Graph()
         G.add_edges_from(self.visual)
-        color_list = ["red", "blue", "red", "blue", "red", "red", "blue"]
-        color_list = ["red", "green", "red", "yellow", "yellow", "green", "red"]
-        # color_list = ["white", "black", "white", "black", "white"]
+        color_list = ["blue", "red", "blue", "red"]
         nx.draw_networkx(G, node_color=color_list, node_size=400, alpha=0.9, with_labels=True,
-                         font_color='black')
+                         font_color='white')
         plt.show()
 
 
 # Driver code
 G = GraphVisualization()
-G.addEdge('D', 'B')
-G.addEdge('B', 'E')
-G.addEdge('A', 'E')
+G.addEdge('B', 'D')
+G.addEdge('D', 'A')
 G.addEdge('A', 'C')
 G.addEdge('C', 'B')
-G.addEdge('E', 'F')
-G.addEdge('F', 'G')
-G.addEdge('G', 'C')
 
 G.visualize()
